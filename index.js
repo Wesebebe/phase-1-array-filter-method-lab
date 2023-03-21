6277b03c2a160cb559dcf84e301c790e871d8988
+ <+>
2023-03-21 22:11:36 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name){
+    for(name of drivers){
+      if(name===name){
+        return name;
+      }
+    }
+}
+drivers.filter(findMatching)
